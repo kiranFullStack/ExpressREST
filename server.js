1d10c1559eff8ca27d867e72a5f5a3d357fce60d
kiranFullStack <kiran.fullstack@outlook.com>
2019-09-11 10:15:29 +0530

    Sending multiple get res

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello from server")
+  res.send("Hello from server home")
+})
+
+app.get("/courses", (req, res) => {
+  res.send("Hello from server All courses")
+})
+
+app.get("/saved", (req, res) => {
+  res.send("Hello from server saved courses")
 })
 
 app.listen(8080, () => console.log("Server Running on 8080🚀🚀🚀🚀🚀🚀🚀🚀🚀"))
