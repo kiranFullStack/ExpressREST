deac2f6ccebbdc4ebc48ec0bb667fcfc01eb45d4
kiranFullStack <kiran.fullstack@outlook.com>
2019-09-11 10:11:08 +0530

    First Response sent from server

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send("Hello from server")
+})
+
 app.listen(8080, () => console.log("Server Running on 8080🚀🚀🚀🚀🚀🚀🚀🚀🚀"))
